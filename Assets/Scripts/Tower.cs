51da8564a001b5ed8b903e589fbd6d4a7f7702ae
colanitre <colanitre@gmail.com>
2020-05-12 17:25:02 +0200

    Added bullets to towers, enemy deaths and more

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -7,10 +7,15 @@ public class Tower : MonoBehaviour
 
     [SerializeField] Transform objectToPan;
     [SerializeField] Transform targetEnemy;
+    [SerializeField] float attackRange = 30f;
+    [SerializeField] GameObject bullet;
+
 
     // Update is called once per frame
     void Update()
     {
         objectToPan.LookAt(targetEnemy);
+        bullet.SetActive(!targetEnemy.Equals(null) && Vector3.Distance(targetEnemy.transform.position, gameObject.transform.position) < attackRange);
+        //bullet.SetActive(targetEnemy.gameObject.activeSelf == true && Vector3.Distance(targetEnemy.position, objectToPan.position) < attackRange);
     }
 }
